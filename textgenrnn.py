e841f13631514d80f312e7550b294dbc0d33eeb5
Max Woolf <max@minimaxir.com>
2017-08-03 08:32:19 -0700

    Rename texts + add model architecture

--- textgenrnn.py
+++ textgenrnn.py
@@ -1,18 +1,17 @@
-import numpy as np
-import csv
-import json
-from random import sample, seed
+from keras.layers import Input, Embedding, LSTM, Dense, GRU
+from keras.optimizers import Nadam, Adam
+from keras.callbacks import ModelCheckpoint, Callback
+from keras.callbacks import LearningRateScheduler, LambdaCallback
+from keras.models import Model, load_model
 from keras.preprocessing import sequence
-from keras.preprocessing.text import text_to_word_sequence, Tokenizer
-from keras.models import Model
-from keras.layers import Input, Embedding, LSTM, Dense, Activation
-from keras.layers.normalization import BatchNormalization
-from keras.optimizers import Adam
-from keras.layers.wrappers import Bidirectional
-from keras.models import load_model
+from random import seed, random, sample
+import numpy as np
+import json
+import h5py
+import csv
 
 
-class docrnn():
+class textgenrnn():
     MAX_LENGTH = 150
     META_TOKEN = '<s>'
     BATCH_SIZE = 128
@@ -31,10 +30,10 @@ class docrnn():
             gen_text = [META_TOKEN] + list(prefix) if prefix else [META_TOKEN]
             next_char = ''
             while next_char is not META_TOKEN:
-                encoded_text = docrnn_encode_sequence(gen_text,
+                encoded_text = textgenrnn_encode_sequence(gen_text,
                                                       self.tokenizer.word_index)
                 probs = self.model.predict(encoded_text)[0]
-                next_char_index = docrnn_sample(probs)
+                next_char_index = textgenrnn_sample(probs)
                 next_char = indices_char[next_char_index]
                 gen_text += [next_char]
             print(gen_text[1:-1])
@@ -48,7 +47,7 @@ class docrnn():
         y = []
 
         for text in texts:
-            subset_x, subset_y = docrnn_encode_training(text, meta_token)
+            subset_x, subset_y = textgenrnn_encode_training(text, meta_token)
             for i in range(len(subset_x)):
                 X.append(subset_x[i])
                 y.append(subset_y[i])
@@ -57,7 +56,7 @@ class docrnn():
         y = np.array(y)
 
         X = self.tokenizer.texts_to_sequences(X)
-        X = sequence.pad_sequences(X, maxlen=MAX_LENGTH+1)
+        X = sequence.pad_sequences(X, maxlen=MAX_LENGTH + 1)
         y = np.array([self.tokenizer.word_index[x] for x in y])
 
         # Append Dense layers to model and retrain.
@@ -83,7 +82,7 @@ class docrnn():
         files = [file_path] if file_path is str else file_path
         texts = []
         for file in files:
-            texts += docrnn_texts_from_file(file, **kwargs)
+            texts += textgenrnn_texts_from_file(file, **kwargs)
         self.train(texts, **kwargs)
 
     def generate_to_file(self, destination_path, **kwargs):
@@ -93,7 +92,29 @@ class docrnn():
                 f.write("{}\n".format(text))
 
 
-def docrnn_sample(preds, temperature):
+def textgenrnn_model(weights_path, num_classes, maxlen=40):
+    '''
+    Builds the model architecture for textgenrnn and
+    loads the pretrained weights.
+    '''
+
+    input = Input(shape=(maxlen,), name='input')
+    embedded = Embedding(num_classes, 100, name='embedding',
+                         input_length=maxlen)(input)
+
+    rnn = GRU(128, return_sequences=False, name='rnn')(embedded)
+
+    output = Dense(num_classes, name='output', activation='softmax')(rnn_1)
+
+    model = Model(inputs=[input], outputs=[output])
+    optimizer = Nadam()
+
+    model.load_weights(weights_path, by_name=True)
+
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+
+
+def textgenrnn_sample(preds, temperature):
     '''
     Samples predicted probabilities of the next character to allow
     for the network to show "creativity."
@@ -107,7 +128,7 @@ def docrnn_sample(preds, temperature):
     return np.argmax(probas)
 
 
-def docrnn_encode_sequence(text, vocab, meta_token='<s>', maxlen=150):
+def textgenrnn_encode_sequence(text, vocab, meta_token='<s>', maxlen=40):
     '''
     Encodes a string into the corresponding encoding for prediction with
     the model.
@@ -117,7 +138,7 @@ def docrnn_encode_sequence(text, vocab, meta_token='<s>', maxlen=150):
     return sequence.pad_sequences(encoded, maxlen=maxlen)
 
 
-def docrnn_encode_training(text, meta_token='<s>', maxlen=150):
+def textgenrnn_encode_training(text, meta_token='<s>', maxlen=40):
     '''
     Encodes a list of texts into a sequence of texts, and the next character
     in those texts.
@@ -134,7 +155,7 @@ def docrnn_encode_training(text, meta_token='<s>', maxlen=150):
     return chars, next_char
 
 
-def docrnn_texts_from_file(file_path, header=True):
+def textgenrnn_texts_from_file(file_path, header=True):
     '''
     Retrieves texts from a newline-delimited file and returns as a list.
     '''
