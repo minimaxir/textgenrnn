8816469a65042030ec11392fd53f1f6bd8920f2c
Max Woolf <max@minimaxir.com>
2017-08-05 18:03:34 -0700

    Make changes for PYPI package compatability

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='textgenrnn',
+    packages=['textgenrnn'],  # this must be the same as the name above
+    version='0.1',
+    description='Pretrained character-based neural network for' \
+    'easily generating text.',
+    author='Max Woolf',
+    author_email='max@minimaxir.com',
+    url='https://github.com/minimaxir/textgenrnn',
+    keywords=['deep learning', 'tensorflow', 'keras', 'text generation'],
+    classifiers=[],
+    license='MIT',
+    include_package_data=True
+)
---
+++ textgenrnn/__init__.py
@@ -0,0 +1,1 @@
+from textgenrnn import textgenrnn
