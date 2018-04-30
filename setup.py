36509dcbc6c19faece3bcd27cc98520897c7f0ef
Max Woolf <max@minimaxir.com>
2018-04-29 20:20:26 -0700

    v1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='textgenrnn',
     packages=['textgenrnn'],  # this must be the same as the name above
-    version='1.0',
+    version='1.1',
     description='Pretrained character-based neural network for ' \
     'easily generating text.',
     author='Max Woolf',
