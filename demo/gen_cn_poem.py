062b521e4272d14c36456b3b57b000ae5b396392
hezhang <hezhang@mobvoi.com>
2018-07-22 18:19:37 +0800

    refine structure

--- demo/gen_cn_poem.py
+++ demo/gen_cn_poem.py
@@ -2,8 +2,8 @@ from textgenrnn import textgenrnn
 
 textgen = textgenrnn(
   name="poem",
-  weights_path='./poem/poem_weights.hdf5',
-  config_path='./poem/poem_config.json',
-  vocab_path='./poem/poem_vocab.json'
+  weights_path='./poem_weights.hdf5',
+  config_path='./poem_config.json',
+  vocab_path='./poem_vocab.json'
 )
 textgen.generate(20, temperature=1.0)
