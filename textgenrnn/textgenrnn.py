6a697ff795f3b8c255da5a014416985d28197390
wolfgang <markus@dathost.net>
2020-01-15 18:59:24 +0100

    Typo

--- textgenrnn/textgenrnn.py
+++ textgenrnn/textgenrnn.py
@@ -1,5 +1,5 @@
 from ensorflow.keras.callbacks import LearningRateScheduler, Callback
-fron tensorflow.keras.models import Model, load_model
+from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.preprocessing.text import Tokenizer, text_to_word_sequence
 from tensorflow.keras.utils import multi_gpu_model
