c7089535c8fd133c0261358a06fcd9a5d062ecd1
wolfgang <markus@dathost.net>
2020-01-15 14:00:35 +0100

    Fix trainable property

--- textgenrnn/AttentionWeightedAverage.py
+++ textgenrnn/AttentionWeightedAverage.py
@@ -22,8 +22,8 @@ class AttentionWeightedAverage(Layer):
 
         self.W = self.add_weight(shape=(input_shape[2], 1),
                                  name='{}_W'.format(self.name),
+                                 trainable=True,
                                  initializer=self.init)
-        self.my_trainable_weights = [self.W]
         super(AttentionWeightedAverage, self).build(input_shape)
 
     def call(self, x, mask=None):
