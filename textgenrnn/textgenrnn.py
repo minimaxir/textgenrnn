cd5967d9760ab7cfcc75ce6ad44add78984b6231
wolfgang <markus@dathost.net>
2020-01-15 19:01:15 +0100

    More typo omg

--- textgenrnn/textgenrnn.py
+++ textgenrnn/textgenrnn.py
@@ -1,4 +1,4 @@
-from ensorflow.keras.callbacks import LearningRateScheduler, Callback
+from tensorflow.keras.callbacks import LearningRateScheduler, Callback
 from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.preprocessing.text import Tokenizer, text_to_word_sequence
